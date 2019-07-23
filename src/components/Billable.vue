8a587ec3463f4a5ec7dbd2254defefb433d1a8b8
William Okafor <wilpat456@gmail.com>
2019-07-23 10:12:02 +0100

    Implements some refactors and doc string

--- src/components/Billable.vue
+++ src/components/Billable.vue
@@ -55,107 +55,148 @@ export default {
     }
   },
   created(){
-        localStorage.setItem("total", 0);
+      // Accumulates the cost of a particular project
+      localStorage.setItem("total", 0);
+  },
+
+  methods: {
+      onFileChange: async function(e) {
+          if(!this.hasExtension('uploader',['.csv'])){
+            this.$notify({
+              group: 'foo',
+              type:'error',
+              title: 'Invalid file type',
+              text: 'Only csv files are allowed!'
+            });
+            return;
+          }
+          var files = e.target.files || e.dataTransfer.files;
+          if (!files.length)
+              return;
+          await this.createInput(files[0]);
       },
 
-        methods: {
-            onFileChange: async function(e) {
-                if(!this.hasExtension('uploader',['.csv'])){
-                  this.$notify({
-                    group: 'foo',
-                    type:'error',
-                    title: 'Invalid file type',
-                    text: 'Only csv files are allowed!'
-                  });
-                  return;
-                }
-                var files = e.target.files || e.dataTransfer.files;
-                if (!files.length)
-                    return;
-                await this.createInput(files[0]);
-            },
-            createInput: async function(file) {
-                var reader = new FileReader();
-                // var vm = this;
-                await reader.readAsDataURL(file);
-                reader.onload = () => {
-                  this.fileUrl = reader.result;
-                  d3.csv(this.fileUrl, (error, data) => {
-                  let allowed_columns = [
-                                      "Billable Rate (per hour)",
-                                      "Date",
-                                      "Employee ID",
-                                      "End Time",
-                                      "Project",
-                                      "Start Time"
-                                     ];
-                  if(data.columns.sort().toString() !== allowed_columns.sort().toString()){
-                    this.$notify({
-                      group: 'foo',
-                      type:'error',
-                      title: 'Invalid content',
-                      text: 'This timesheet doesn\'t follow the accepted structure.'
-                    });
-                    return;
-                  }
-                  const replacements = {
-                  'Employee ID': 'employee_id', 
-                  'Billable Rate (per hour)': 'rate_per_hour',
-                  'Project': 'project',
-                  'Date': 'date',
-                  'Start Time': 'start_time',
-                  'End Time': 'end_time'};
-                  let projects = [];
-                    let newData = data.map(row =>{
-                      projects.push(row.Project);
-                    let replacedItems = Object.keys(row).map((key) => {
-                      const newKey = replacements[key] || key;
-                      return { [newKey] : row[key] };
-                    });
-                    const newRow = Object.assign({}, ...replacedItems);
-                    return newRow;
-                  });
-                  this.projects = new Set(projects);
-                  this.rowData = newData;
-                  this.file_name = file.name;
-                  this.button_text = 'Choose another'
-                  });
-                }
-            },
-
-            time_difference: (a,b) =>{
-              let time1 = a.split(':');
-              let time2 = b.split(':');
-              let date1 = new Date(0, 0, 0, time1[0], time1[1]);
-            let date2 = new Date(0, 0, 0, time2[0], time2[1]);
-            var difference = new Date(date2 - date1);
-            return difference.getUTCHours();
-            },
-            get_cost: function(rate, start_time, end_time){
-              var hours = this.time_difference(start_time, end_time);
-              var cost = rate * hours;
-              var current_total = parseInt(localStorage.getItem('total'));
-              current_total += cost;
-              localStorage.setItem('total', current_total);
-              return cost;
-            },
-            get_total: function(){
-              let total = localStorage.getItem('total');
-              localStorage.setItem('total', 0);
-              return total;
-            },
-            get_bills: function(project){
-              return this.rowData.filter(i => i.project === project)
-            },
-            upload: () =>{
-              document.getElementById('uploader').click();
-            },
-            hasExtension: (inputID, exts) =>{
-                var fileName = document.getElementById(inputID).value;
-                return (new RegExp('(' + exts.join('|').replace(/\./g, '\\.') + ')$')).test(fileName);
+      /**
+       * Reads the selected file and populates the variables needed for rendering
+       */
+      createInput: async function(file) {
+          var reader = new FileReader();
+          await reader.readAsDataURL(file);
+          reader.onload = () => {
+            this.fileUrl = reader.result;
+            d3.csv(this.fileUrl, async (error, data) => {
+            let allowed_columns = [
+                                "Billable Rate (per hour)",
+                                "Date",
+                                "Employee ID",
+                                "End Time",
+                                "Project",
+                                "Start Time"
+                               ];
+            if(data.columns.sort().toString() !== allowed_columns.sort().toString()){
+              this.$notify({
+                group: 'foo',
+                type:'error',
+                title: 'Invalid content',
+                text: 'This timesheet doesn\'t follow the accepted structure.'
+              });
+              return;
             }
+            let { cleaned_data, projects }  = await this.preprocess(data);
+            this.projects = new Set(projects);
+            this.rowData = cleaned_data;
+            this.file_name = file.name;
+            this.button_text = 'Choose another'
+            });
+          }
+      },
+      /**
+       * Cleans up the data for rendering
+       * @param {object} data
+       * @return {object} cleaned data, [array] projects
+       */
+      preprocess: (data) => {
+        const replacements = {
+        'Employee ID': 'employee_id', 
+        'Billable Rate (per hour)': 'rate_per_hour',
+        'Project': 'project',
+        'Date': 'date',
+        'Start Time': 'start_time',
+        'End Time': 'end_time'};
+        let projects = [];
+        let cleaned_data = data.map(row =>{
+          projects.push(row.Project);
+          let replacedItems = Object.keys(row).map((key) => {
+            const newKey = replacements[key] || key;
+            return { [newKey] : row[key] };
+          });
+          const newRow = Object.assign({}, ...replacedItems);
+          return newRow;
+        });
+        return {cleaned_data, projects};
+      },
 
-        }
+      /**
+       * Gets the difference in hours between a start and end time
+       * @return 'string'
+       */
+      time_difference: (a,b) =>{
+        let time1 = a.split(':');
+        let time2 = b.split(':');
+        let date1 = new Date(0, 0, 0, time1[0], time1[1]);
+        let date2 = new Date(0, 0, 0, time2[0], time2[1]);
+        var difference = new Date(date2 - date1);
+        return difference.getUTCHours();
+      },
+
+       /**
+       * Gets the billable cost for the elapsed hours
+       * @return 'int'
+       */
+      get_cost: function(rate, start_time, end_time){
+        var hours = this.time_difference(start_time, end_time);
+        var cost = rate * hours;
+        var current_total = parseInt(localStorage.getItem('total'));
+        current_total += cost;
+        localStorage.setItem('total', current_total);
+        return cost;
+      },
+
+      /**
+       * Gets the total cost billable for a project
+       * @return 'string'
+       */
+      get_total: function(){
+        let total = localStorage.getItem('total');
+        localStorage.setItem('total', 0);
+        return total;
+      },
+
+      /**
+       * Gets the recorded timesheets of a particular project
+       * @return [array]
+       */
+      get_bills: function(project){
+        return this.rowData.filter(i => i.project === project)
+      },
+
+      /**
+       * Activates the file picker
+       */
+      upload: () =>{
+        document.getElementById('uploader').click();
+      },
+
+      /**
+       * Validates if the entered file is csv or not
+       * @return bool
+       */
+      hasExtension: (inputID, exts) =>{
+          var fileName = document.getElementById(inputID).value;
+          return (new RegExp('(' + exts.join('|').replace(/\./g, '\\.') + ')$')).test(fileName);
+      }
+
+  }
 }
 </script>
 
